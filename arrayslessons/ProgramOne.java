950425eee8080a28b161a5b2fe376f6d469bca63
heinsattnaing <heinsatt23@gmail.com>
2025-09-03 21:25:14 +0630

    Initial commit

---
+++ arrayslessons/ProgramOne.java
@@ -0,0 +1,9 @@
+package arrayslessons;
+
+public class ProgramOne {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        System.out.println("This is Hein Satt Naing Speaking!");
+    }
+}
